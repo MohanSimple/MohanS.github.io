a5fdcb6d6ee0b0b148197aae9ebdde5ad864f492
Mohan S <mr.mohans@icloud.com>
2020-05-02 22:06:10 +0530

    Profile

---
+++ Profile.js
@@ -0,0 +1,99 @@
+const canvas= document.querySelector('canvas');
+const c = canvas.getContext("2d");
+
+canvas.width =window.innerWidth;
+canvas.height =window.innerHeight;
+
+
+const mouse={
+    x: undefined,
+    y: undefined,
+}
+
+var minRadius=2;
+var maxRadius=30;
+
+colorArray=[
+    "maroon",
+    "indianred",
+    "darkgrey",
+    "white",
+    "darkslategrey",
+    "darkslategray"
+]
+
+window.addEventListener('mousemove', function(event) { 
+    mouse.x=event.x;
+    mouse.y=event.y;
+    this.console.log(event.x);
+});
+
+window.addEventListener('resize', () => {
+    canvas.width=innerWidth
+    canvas.height=innerHeight
+
+    init()
+})
+
+function Circle(x,y, radius, color){
+    this.x=x
+    this.y=y
+    this.radius=radius
+    this.minRadius=radius;
+    this.color=colorArray[Math.floor(Math.random()*colorArray.length)]
+    this.velocity={
+        x:Math.random() - 0.5,
+        y: Math.random() - 0.5
+    }
+}
+
+
+Object.prototype.draw = function() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false)
+    c.fillStyle=this.color
+    c.fill()
+    c.closePath()
+}
+
+
+Object.prototype.update = function(){
+    
+
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+
+    if(mouse.x -this.x < 50 && mouse.x -this.x > -50 && mouse.y -this.y < 50 && mouse.y -this.y > -50 && this.radius < maxRadius){
+        this.radius+=1;
+    }
+    else if( this.radius > minRadius){
+        this.radius=minRadius;
+    }
+    this.draw();
+}
+
+
+let circles   = [];
+function init(){
+    circles=[];
+    for(let i=0; i<800 ; i++){
+        
+        const x=Math.random()*canvas.width
+        const y= Math.random() * canvas.height
+        const radius=Math.random()*5
+        const color ='darkslategray';
+        circles.push(new Circle(x, y, radius, color))
+    }
+}
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+
+    circles.forEach(circle =>{
+        circle.update()
+    })
+} 
+
+init()
+animate()
